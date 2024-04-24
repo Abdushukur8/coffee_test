fdf19b4fefa203a49fd610e880867916f2e48a6d
Shukur <abdusukurmahmudov14@gmail.com>
2024-04-24 14:01:20 +0500

    create slider

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// BURGER start
+
+const navbar = document.querySelector('.navbar')
+const burger = document.querySelector('.header__burger')
+
+function openBurger(){
+    navbar.classList.toggle('bur_open')
+}
+burger.addEventListener('click', openBurger)
+// BURGER end
+
+
+const searchHeader = document.querySelector('.header_search')
+const searIcon = document.querySelector('.icon_search')
+searIcon.addEventListener('click', function(){
+    searchHeader.classList.toggle('header_search_active')
+})
+
+
+const images = document.querySelectorAll('.slider-img')
+
+const slideControls = document.querySelectorAll('.slideBtn')
+
+let imageIndex = 0
+function show(index) {
+    images[imageIndex].classList.remove('active')
+    images[index].classList.add('active')
+    imageIndex = index
+
+}
+
+slideControls.forEach((e) => {
+    e.addEventListener('click', () =>{
+        if (event.target.classList.contains('prevBtn')){
+            let index = imageIndex - 1
+            if(index < 0){
+                index = images.length - 1
+            }
+            show(index)
+        }else if (event.target.classList.contains('nextBtn')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+show(imageIndex)
